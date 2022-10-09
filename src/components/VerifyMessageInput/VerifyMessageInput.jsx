7a4957170d7a37346ef81176161e3e8ef4e72d34
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2022-10-09 19:11:39 -0400

    add animation to verify messag input

--- src/components/VerifyMessageInput/VerifyMessageInput.jsx
+++ src/components/VerifyMessageInput/VerifyMessageInput.jsx
@@ -3,7 +3,12 @@ import { motion } from 'framer-motion'
 
 export const VerifyMessageInput = () => {
     return (
-        <motion.div>
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className='container-verify-message'>
+            
             <input id='signature' placeholder='Signature'></input>
             <textarea id='message' placeholder='Message to Verify' rows='5' cols='50'></textarea>
             <button onClick={() => verifyMessage(
