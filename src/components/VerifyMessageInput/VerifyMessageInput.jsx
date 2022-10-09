3b048de999267283d1d41461cf48d92b56ec600f
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2022-10-09 16:14:31 -0400

    first pass of verify message component

--- src/components/VerifyMessageInput/VerifyMessageInput.jsx
+++ src/components/VerifyMessageInput/VerifyMessageInput.jsx
@@ -1,7 +1,15 @@
+import { verifyMessage } from 'ethers/lib/utils'
+import { motion } from 'framer-motion'
+
 export const VerifyMessageInput = () => {
     return (
-        <div>
-            Verify input
-        </div>
+        <motion.div>
+            <input id='signature' placeholder='Signature'></input>
+            <textarea id='message' placeholder='Message to Verify' rows='10' cols='50'></textarea>
+            <button onClick={() => verifyMessage(
+                document.querySelector('#message').value,
+                document.querySelector('#signature').value
+            )}>Verify Message</button>
+        </motion.div>
     )
 }
