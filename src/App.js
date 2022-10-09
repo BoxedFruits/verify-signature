f172cedb5b4820fdbaf91ad2b8099a120fab2a28
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2022-10-08 23:16:12 -0400

    place holder effects

--- src/App.js
+++ src/App.js
@@ -16,13 +16,16 @@ export const App = () => {
             <motion.div
                 className='blue-gradient'
             >
-                <motion.p
-                    onTap={() => {currMode !== MODES.Sign ? setCurrMode(MODES.Sign) : setCurrMode(MODES.Select)}}
-                    initial={{ opacity: 1 }}
-                    whileTap={{ scale: 1.17 }}
-                    whileHover={{ scale: 1.1 }}
-                    animate={currMode === MODES.Verify ? { opacity: 0 } : { opacity: 1 }}
-                >SIGN MESSAGE</motion.p>
+                {currMode === MODES.Select || currMode === MODES.Sign ?
+                    <motion.p
+                        onTap={() => { currMode !== MODES.Sign ? setCurrMode(MODES.Sign) : setCurrMode(MODES.Select) }}
+                        initial={{ opacity: 1 }}
+                        whileTap={{ scale: 1.17 }}
+                        whileHover={{ scale: 1.1 }}
+                        animate={currMode === MODES.Verify ? { opacity: 0 } : { opacity: 1 }}
+                    >SIGN MESSAGE</motion.p>
+                    : <div>input box</div>
+                }
             </motion.div>
             <div
                 className='middle'
@@ -32,13 +35,16 @@ export const App = () => {
             <motion.div
                 className='red-gradient'
             >
-                <motion.p
-                    onTap={() => {currMode !== MODES.Verify ? setCurrMode(MODES.Verify): setCurrMode(MODES.Select)}}
-                    initial={{ opacity: 1 }}
-                    whileTap={{ scale: 1.17 }}
-                    whileHover={{ scale: 1.1 }}
-                    animate={currMode === MODES.Sign ? { opacity: 0 } : { opacity: 1 }}
-                >VERIFY MESSAGE</motion.p>
+                {currMode === MODES.Select || currMode === MODES.Verify ?
+                    <motion.p
+                        onTap={() => { currMode !== MODES.Verify ? setCurrMode(MODES.Verify) : setCurrMode(MODES.Select) }}
+                        initial={{ opacity: 1 }}
+                        whileTap={{ scale: 1.17 }}
+                        whileHover={{ scale: 1.1 }}
+                        animate={currMode === MODES.Sign ? { opacity: 0 } : { opacity: 1 }}
+                    >VERIFY MESSAGE</motion.p>
+                    : <div>input box</div>
+                }
             </motion.div>
         </div>
     );
