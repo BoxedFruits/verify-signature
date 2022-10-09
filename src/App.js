e18cdb27616003c74915e291cc2dcfabc6e932dc
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2022-10-09 15:39:30 -0400

    add WagmiConfig

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import './App.css';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { useState } from 'react';
+import { SignMessageInput } from './components/SignMessageInput/SignMessageInput';
+import {
+    WagmiConfig,
+    createClient,
+    configureChains,
+    defaultChains,
+} from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+
 
 const MODES = {
     Sign: 'Sign',
@@ -9,44 +18,63 @@ const MODES = {
 }
 
 export const App = () => {
+    const { chains, provider, webSocketProvider } = configureChains(
+        defaultChains,
+        [publicProvider()],
+    )
+
+    const client = createClient({
+        autoConnect: true,
+        provider,
+        webSocketProvider,
+    });
+
     const [currMode, setCurrMode] = useState(MODES.Select);
 
     return (
-        <div>
-            <motion.div
-                className='blue-gradient'
-            >
-                {currMode === MODES.Select || currMode === MODES.Sign ?
-                    <motion.p
-                        onTap={() => { currMode !== MODES.Sign ? setCurrMode(MODES.Sign) : setCurrMode(MODES.Select) }}
-                        initial={{ opacity: 1 }}
-                        whileTap={{ scale: 1.17 }}
-                        whileHover={{ scale: 1.1 }}
-                        animate={currMode === MODES.Verify ? { opacity: 0 } : { opacity: 1 }}
-                    >SIGN MESSAGE</motion.p>
-                    : <div>input box</div>
-                }
-            </motion.div>
-            <div
-                className='middle'
-            >
-                <hr></hr>
+        <WagmiConfig client={client}>
+            <div>
+                <motion.div
+                    className='blue-gradient'
+                >
+                    {currMode === MODES.Select || currMode === MODES.Sign ?
+                        <AnimatePresence>
+                            <motion.p
+                                onTap={() => { currMode !== MODES.Sign ? setCurrMode(MODES.Sign) : setCurrMode(MODES.Select) }}
+                                initial={{ opacity: 0 }}
+                                whileTap={{ scale: 1.17 }}
+                                whileHover={{ scale: 1.1 }}
+                                animate={currMode === MODES.Verify ? { opacity: 0 } : { opacity: 1 }}
+                                exit={{ opacity: 0 }}
+                            >SIGN MESSAGE</motion.p>
+                        </AnimatePresence>
+                        : <AnimatePresence>
+                            <SignMessageInput
+                            ></SignMessageInput>
+                        </AnimatePresence>
+                    }
+                </motion.div>
+                <div
+                    className='middle'
+                >
+                    <hr></hr>
+                </div>
+                <motion.div
+                    className='red-gradient'
+                >
+                    {currMode === MODES.Select || currMode === MODES.Verify ?
+                        <motion.p
+                            onTap={() => { currMode !== MODES.Verify ? setCurrMode(MODES.Verify) : setCurrMode(MODES.Select) }}
+                            initial={{ opacity: 1 }}
+                            whileTap={{ scale: 1.17 }}
+                            whileHover={{ scale: 1.1 }}
+                            animate={currMode === MODES.Sign ? { opacity: 0 } : { opacity: 1 }}
+                        >VERIFY MESSAGE</motion.p>
+                        : <div>input box</div>
+                    }
+                </motion.div>
             </div>
-            <motion.div
-                className='red-gradient'
-            >
-                {currMode === MODES.Select || currMode === MODES.Verify ?
-                    <motion.p
-                        onTap={() => { currMode !== MODES.Verify ? setCurrMode(MODES.Verify) : setCurrMode(MODES.Select) }}
-                        initial={{ opacity: 1 }}
-                        whileTap={{ scale: 1.17 }}
-                        whileHover={{ scale: 1.1 }}
-                        animate={currMode === MODES.Sign ? { opacity: 0 } : { opacity: 1 }}
-                    >VERIFY MESSAGE</motion.p>
-                    : <div>input box</div>
-                }
-            </motion.div>
-        </div>
+        </WagmiConfig>
     );
 }
 
