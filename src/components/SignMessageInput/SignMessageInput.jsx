de6e002e9092960d4d4d56b959f0bf9adf0a0dff
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2022-10-09 21:31:34 -0400

    style container

--- src/components/SignMessageInput/SignMessageInput.jsx
+++ src/components/SignMessageInput/SignMessageInput.jsx
@@ -37,9 +37,9 @@ export const SignMessageInput = () => {
                     </button>
                 )) : 
                     (
-                        <motion.div>
+                        <motion.div id="sign-message-container">
                             <textarea id="message" placeholder='Message to Sign' rows="5" cols="50"></textarea>
-                            <button onClick={() => sign()}>Sign Message</button>
+                            <button className="btn" onClick={() => sign()}>Sign Message</button>
                         </motion.div>
                        
                     )
