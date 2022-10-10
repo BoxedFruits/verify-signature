5a8996a729d87ffa6c7fc79315d5b881e707b712
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2022-10-09 21:45:12 -0400

    show signed of message, given a signature

--- src/components/VerifyMessageInput/VerifyMessageInput.jsx
+++ src/components/VerifyMessageInput/VerifyMessageInput.jsx
@@ -1,20 +1,31 @@
 import { verifyMessage } from 'ethers/lib/utils'
 import { motion } from 'framer-motion'
+import { useState } from 'react'
 
 export const VerifyMessageInput = () => {
+    const [signerAddress, setSignerAddress] = useState();
+    const _verifyMessage = () => {
+        //Need error handling
+        setSignerAddress(verifyMessage(
+            document.querySelector('#message').value,
+            document.querySelector('#signature').value
+        ));
+    }
+
     return (
         <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             className='container-verify-message'>
-            
             <input id='signature' placeholder='Signature'></input>
+            
             <textarea id='message' placeholder='Message to Verify' rows='5' cols='50'></textarea>
-            <button className="btn" onClick={() => verifyMessage(
-                document.querySelector('#message').value,
-                document.querySelector('#signature').value
-            )}>Verify Message</button>
+            
+            <button className="btn" onClick={() => _verifyMessage()}>Verify Message</button>
+            
+            {signerAddress && <div>The address that signed the message is: {signerAddress}</div>}
         </motion.div>
+        
     )
 }
